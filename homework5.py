36ac7440588c87cc310bffa4c6f1291ba2e90a05
Aferist958 <141312413+Aferist958@users.noreply.github.com>
2024-05-05 12:02:44 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['banana', ' cocount', 'apple', 'orange', 'mandarie']
+print(my_list)
+print(my_list[0], my_list[4])
+print(my_list[2:4])
+my_list[2] = 'pear'
+print(my_list)
+
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин'}
+print(my_dict)
+print(my_dict['apple'])
+my_dict['kiwi'] = 'киви'
+print(my_dict)
