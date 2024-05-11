03e5dea232cf94bb054466803c798ed4c6f7ac37
Aferist958 <141312413+Aferist958@users.noreply.github.com>
2024-05-11 10:29:40 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,7 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = list(students)
+students.sort()
+dict_ = dict(zip(students, grades))
+dict_variable = {key:sum(value)/len(value) for key, value in dict_.items()}
+print(dict_variable)
